5dba8a9866daeb753f13ea7fbde0462c3ed51a27
FabienBrG <fabien.berger@ymail.com>
2014-12-25 14:08:31 +0100

    [UPDATE] Fonctionnalité Ajouter Mat (tab dynamique)
    J'ai fais les tableaux dynamique lier au pop des onglets qui demande
    d'ajouter un élément ! (carac log, user & maintenance) !

--- Symfony2/web/js/script.js
+++ Symfony2/web/js/script.js
@@ -7,3 +7,79 @@ function change_onglet(name) {
     anc_onglet = name;
 }
 //-->
+
+function recupSaisiePopupUser(){
+    
+    var tableau = document.getElementById("tabUser");
+    
+    var ligne = tableau.insertRow(-1);//on a ajouté une ligne
+
+    var colonne = ligne.insertCell(0);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("nomUser").value;//on y met le contenu de titre
+    
+    var colonne2 = ligne.insertCell(1);
+    colonne2.innerHTML += '<a onclick="supprimerLigneUser(this)">X</a>';  
+}
+
+function recupSaisiePopupLog(){
+    
+    var tableau = document.getElementById("tabLog");
+    
+    var ligne = tableau.insertRow(-1);//on a ajouté une ligne
+
+    var colonne = ligne.insertCell(0);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("form_editeur").value;//on y met le contenu de titre
+    
+    var colonne2 = ligne.insertCell(1);//on a une ajouté une cellule
+    colonne2.innerHTML += document.getElementById("form_nomLog").value;//on y met le contenu de titre
+    
+    var colonne3 = ligne.insertCell(2);//on a une ajouté une cellule
+    colonne3.innerHTML += document.getElementById("form_licence").value;//on y met le contenu de titre
+    
+    var colonne4 = ligne.insertCell(3);
+    colonne4.innerHTML += '<a onclick="supprimerLigneLog(this)">X</a>'; 
+}
+
+function recupSaisiePopupMaintenance(){
+    
+    var tableau = document.getElementById("tabMaintenance");
+    
+    var ligne = tableau.insertRow(-1);//on a ajouté une ligne
+
+    var colonne = ligne.insertCell(0);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("dateInterv").value;//on y met le contenu de titre
+    
+    var colonne = ligne.insertCell(1);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("objInterv").value;//on y met le contenu de titre
+    
+    var colonne = ligne.insertCell(2);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("descInterv").value;//on y met le contenu de titre
+    
+    var colonne = ligne.insertCell(3);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("prestaInterv").value;//on y met le contenu de titre
+    
+    var colonne = ligne.insertCell(4);//on a une ajouté une cellule
+    colonne.innerHTML += document.getElementById("coutInterv").value;//on y met le contenu de titre
+    
+    var colonne2 = ligne.insertCell(5);
+    colonne2.innerHTML += '<a onclick="supprimerLigneMaintenance(this)">X</a>';  
+}
+
+function supprimerLigneMaintenance(r)
+{
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById('tabMaintenance').deleteRow(i-1);
+}
+
+function supprimerLigneUser(r)
+{
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById('tabUser').deleteRow(i-1);
+}
+
+function supprimerLigneLog(r)
+{
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById('tabLog').deleteRow(i-1);
+}
+
